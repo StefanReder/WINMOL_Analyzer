7d67df6b5bdd9bc595d2f7048134e02ac8ba152a
Andre Henn <henn@terrestris.de>
2023-11-29 17:07:57 +0100

    apply flake8 for qgis plugin files

--- winmol_analyzer_dialog.py
+++ winmol_analyzer_dialog.py
@@ -25,8 +25,7 @@ Dialog
 
 import os
 
-from qgis.PyQt import uic
-from qgis.PyQt import QtWidgets
+from qgis.PyQt import QtWidgets, uic
 
 # This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
