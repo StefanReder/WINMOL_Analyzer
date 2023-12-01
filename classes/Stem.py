aa4631c13e3ede292272c4423fe121c4c77a6090
Andre Henn <henn@terrestris.de>
2023-12-01 16:05:45 +0100

    feat: introduce dataclasses for nodes and vectors

--- classes/Stem.py
+++ classes/Stem.py
@@ -4,9 +4,13 @@ from typing import List, Tuple
 
 from shapely import LineString, Point
 
+from classes.Node import Node
+from classes.Vector import Vector
+
 
 @dataclass
 class Stem:
+    stem_id: int
     start: Point
     stop: Point
     path: LineString
@@ -14,8 +18,6 @@ class Stem:
     segment_diameter_list: List[float]
     segment_length_list: List[float]
     segment_volume_list: List[float]
-    length: float
-    volume: float
 
     def __eq__(self, other):
         return (self.start == other.start and self.stop == other.stop
@@ -27,3 +29,44 @@ class Stem:
              'stop', tuple(list(self.stop.coords)),
              'path', tuple(list(self.path.coords)))
         )
+
+    def length(self):
+        if len(self.segment_length_list) == 0:
+            return 0
+        return sum(self.segment_length_list)
+
+    def volume(self):
+        if len(self.segment_volume_list) == 0:
+            return 0
+        return sum(self.segment_volume_list)
+
+    # TBD: what's the difference between nodes and vectors beside the geometry?
+    def get_nodes(self) -> List[Node]:
+        if self.path.coords is None:
+            return []
+        node_list = []
+        for j in range(len(self.path.coords)):
+            node_list.append(Node(
+                diameter=self.segment_diameter_list[j],
+                geom=Point(self.path.coords[j]),
+                # TODO: check this
+                vector=self.vector,
+                stem_id=self.stem_id,
+                node_id=j
+            ))
+        return node_list
+
+    def get_vectors(self) -> List[Vector]:
+        if self.path.coords is None:
+            return []
+        vector_list = []
+        for j in range(len(self.path.coords)):
+            vector_list.append(Vector(
+                diameter=self.segment_diameter_list[j],
+                geom=self.path,
+                # TODO: check this
+                vector=self.vector,
+                stem_id=self.stem_id,
+                node_id=j
+            ))
+        return vector_list
